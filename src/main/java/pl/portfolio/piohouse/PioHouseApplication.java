7e45dba102b2ee540cca2e18577fe885f746191e
Majster96PL <piotruniok96@gmail.com>
2023-12-31 21:02:55 +0100

    Initial commit

---
+++ src/main/java/pl/portfolio/piohouse/PioHouseApplication.java
@@ -0,0 +1,13 @@
+package pl.portfolio.piohouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/portfolio/piohouse/PioHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.portfolio.piohouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PioHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
