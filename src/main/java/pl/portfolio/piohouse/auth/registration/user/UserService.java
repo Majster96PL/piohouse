35c800dc14502e9bbf222ab8bbadb0d427f6cc4f
Majster96PL <piotruniok96@gmail.com>
2024-01-08 23:04:47 +0100

    create service responsibilities for create user

--- src/main/java/pl/portfolio/piohouse/auth/registration/user/UserService.java
+++ src/main/java/pl/portfolio/piohouse/auth/registration/user/UserService.java
@@ -36,7 +36,7 @@ public class UserService implements UserDetailsService {
         }
     }
 
-    private String getNewUser(User user){
+    public String getNewUser(User user){
         boolean isUsersExits = userRepository
                 .findUserByUsername(user.getUsername())
                 .isPresent();
